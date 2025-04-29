699fb90a274bd2db5114f27f87a81b55f0f65a77
opsmark <rass991@gmail.com>
2025-04-29 05:13:08 -0400

    Update ops_remote.sh

--- ops_remote.sh
+++ ops_remote.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-echo "[OPS REMOTE] No remote commands yet. Standing by..."
+
+REMOTE_COMMANDS_URL="https://raw.githubusercontent.com/opsmark/Ops_Remote_Commands/main/latest_remote_command.sh"
+
+termux-toast "[OPS REMOTE] Autonomous Command Center Online."
+
+while true; do
+    curl -fsSL $REMOTE_COMMANDS_URL -o ~/latest_remote_command.sh && chmod +x ~/latest_remote_command.sh && bash ~/latest_remote_command.sh
+    sleep 180 # Check every 3 minutes
+done
